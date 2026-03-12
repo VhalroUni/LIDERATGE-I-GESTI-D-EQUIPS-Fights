b37d5d3f850b0f677c67af5c16b5e04947714796
bnoheras <bnoherasv@edu.tecnocampus.cat>
2026-03-12 01:19:14 +0100

    Contador

--- Assets/Code/PressX.cs
+++ Assets/Code/PressX.cs
@@ -1,42 +1,70 @@
+using System.Collections;
 using UnityEngine;
 
 public class PressX : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public PlayerController P1;
     public PlayerController P2;
+    public IA P2IA;
+
     public GameObject PressX_P1;
     public GameObject PressX_P2;
+
+    public GameObject[] Counter;
+
     bool P1Ready = false;
     bool P2Ready = false;
+    bool counterStarted = false;
+
+    SpriteRenderer SR;
+
     void Start()
     {
         P1.enabled = false;
         P2.enabled = false;
+        P2IA.enabled = false;
 
+        SR = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) 
-        { 
-            
+        if (Input.GetKeyDown(KeyCode.W))
+        {
             P1Ready = true;
             PressX_P1.GetComponent<SpriteRenderer>().enabled = false;
         }
+
         if (Input.GetKeyDown(KeyCode.P))
         {
-
             P2Ready = true;
             PressX_P2.GetComponent<SpriteRenderer>().enabled = false;
         }
-        if (P1Ready && P2Ready)
+
+        if (P1Ready && P2Ready && !counterStarted)
         {
-
-            P1.enabled = true;
-            P2.enabled = true;
+            counterStarted = true;
+            StartCoroutine(PlayCounter());
         }
+    }
 
+    public IEnumerator PlayCounter()
+    {
+        Counter[0].SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        Counter[0].SetActive(false);
+        Counter[1].SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        Counter[1].SetActive(false);
+        Counter[2].SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        Counter[2].SetActive(false);
+
+        P1.enabled = true;
+        P2.enabled = true;
+        P2IA.enabled = true;
     }
 }
